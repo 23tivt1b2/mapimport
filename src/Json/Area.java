64457db121c4ab1181dd84514b579a3a7d7b2ab0
EricVeen <ericveen@hotmail.nl>
2019-03-18 15:20:34 +0100

    objects

---
+++ src/Json/Area.java
@@ -0,0 +1,62 @@
+package Json;
+
+public class Area {
+    private double height;
+    private int id;
+    private String name;
+    private double width;
+    private double x;
+    private double y;
+
+    public Area(){
+
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
